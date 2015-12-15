c45a94fb09f23743c1ca3df111a5186e993cedaf
Talbert <mtalbert@usgs.gov>
2015-12-15 08:24:23 -0700

    reactive map components updates working

--- MyLeafletRasterExample.R
+++ MyLeafletRasterExample.R
@@ -7,7 +7,7 @@ ui <- bootstrapPage(
   leafletOutput("map1", width = "100%", height = "50%"),
   leafletOutput("map2",width="50%",height="25%"),
   leafletOutput("map3",width="50%",height="25%"),
-  absolutePanel(top = 10, left = 10,
+  absolutePanel(top = 30, left = 10,
                        selectInput("mapVar", "Variable",
                             choices=c("Precipitation","Temperature","Elevation"),
                             selected="Temperature")
@@ -15,31 +15,51 @@ ui <- bootstrapPage(
 )
 
 server <- function(input, output, session) {
- 
+  MapSpecs <- reactiveValues(
+    i = 2,
+    MapCols = "OrRd",
+    Label = "Average Annual Temperature"
+  )
+  Mapi=reactive({   
+     switch(input$mapVar,
+                  Temperature = 1,
+                  Precipitation = 2,
+                  Elevation =3)})
+  MapCols=reactive({
+      switch(input$mapVar,
+                         Temperature = "OrRd",
+                         Precipitation = "BuGn",
+                         Elevation ="Spectral")
+               
+  
+   })
+  MapLab=reactive({
+    switch(input$mapVar,
+           Temperature = "Average Annual Temperature",
+           Precipitation = "Total Annual Precipitation",
+           Elevation ="Elevation")
+    
+    
+  })
+  
   output$map1 <- renderLeaflet({
     # Use leaflet() here, and only include aspects of the map that
     # won't need to change dynamically (at least, not unless the
     # entire map is being torn down and recreated).
    
-    if(input$mapVar == "Temperature") pal = colorNumeric("OrRd", values(r),
-              na.color = "transparent")
-   
-    if(input$mapVar == "Precipitation") pal =  colorNumeric("BuGn", values(r),
-                                               na.color = "transparent")
+    MCols <- MapCols()
+    MLab <-MapLab()
+    Mi<-Mapi()
     
-      
-    if(input$mapVar == "Elevation")    pal = colorNumeric("Spectral", values(r),
-                                                na.color = "transparent")
+    pal = colorNumeric(MCols, values(r),
+              na.color = "transparent")
    
     leaflet() %>% addTiles() %>%  addRasterImage(r, colors = pal, opacity = 0.8) %>%
       addLegend(pal = pal, values = values(r),
-                title = "Surface temp")
+                title = MLab)
   })
 }  
-  # Incremental changes to the map (in this case, replacing the
-  # circles when a new color is chosen) should be performed in
-  # an observer. Each independent set of things that can change
-  # should be managed in its own observer.
+
  
  
 

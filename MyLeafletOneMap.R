eeafb2535741bb1ba93238716d73f4d4c530cf56
Talbert <mtalbert@usgs.gov>
2015-12-22 11:26:13 -0700

    working single map with updating rasters trying to add shape now

---
+++ MyLeafletOneMap.R
@@ -0,0 +1,58 @@
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+load("C:\\Users\\mtalbert\\Desktop\\Climate\\ParkOutput\\UnitedStates\\USMaps")
+
+ui <- bootstrapPage(
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("Map", width = "100%", height = "50%"),
+   absolutePanel(top = "40%", left = 10,
+                       selectInput("mapVar", "Variable",
+                            choices=c("Precipitation","Temperature","Elevation"),
+                            selected="Temperature")
+  )
+)
+
+server <- function(input, output, session) {
+  
+  Mapi=reactive({   
+     switch(input$mapVar,
+                  Temperature = 1,
+                  Precipitation = 2,
+                  Elevation =3)})
+  MapCols=reactive({
+      switch(input$mapVar,
+                         Temperature = "OrRd",
+                         Precipitation = "BuGn",
+                         Elevation ="Spectral")})
+  MapLab=reactive({
+    switch(input$mapVar,
+           Temperature = "Average Annual Temperature",
+           Precipitation = "Total Annual Precipitation",
+           Elevation ="Elevation")})
+  Mi<-isolate(Mapi())
+  MapLst<-reactive({
+    if(input$mapVar=="Temperature") return(ShinyMapLst[[1]])
+    if(input$mapVar=="Precipitation") return(ShinyMapLst[[2]])
+     })
+  
+    output$Map <- renderLeaflet({
+      dataset <- MapLst()
+      leaflet() %>% addTiles() %>%  addRasterImage(dataset[[1]], colors = MapCols(), opacity = 0.8) %>%
+        addLegend(pal = pal, values = values(dataset[[1]]),
+                  title = "Map")
+      #for(i in 1:length(Bounds@polygons[[1]]@Polygons)){
+       # coords<-Bounds@polygons[[1]]@Polygons[[i]]@coords
+       # MyMap<- addPolygons(MyMap,
+       #                     lat=coords[,2],
+        #                    lng=coords[,1],
+        #                    fill=FALSE,
+        #                    layerId=as.character(i))
+      
+      #} 
+    })
+  
+
+}  
+
+shinyApp(ui, server)
